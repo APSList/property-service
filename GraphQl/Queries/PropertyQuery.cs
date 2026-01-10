0c057f6bf8b59d753fb434ca9e3a93c80ea4cd00
anzeljc7 <tilen.anzeljc7@gmail.com>
2026-01-10 12:55:32 +0100

    Graphql fix

--- GraphQl/Queries/PropertyQuery.cs
+++ GraphQl/Queries/PropertyQuery.cs
@@ -1,4 +1,5 @@
 ﻿using HotChocolate.Authorization;
+using System.Security.Claims;
 using property_service.Interfaces;
 using property_service.Models.PropertyModels;
 
@@ -6,10 +7,26 @@ namespace property_service.GraphQl.Queries;
 
 public class PropertyQuery
 {
+    [UseFiltering]
+    [UseSorting]
     [Authorize(Policy = "OrgRequired")]
-    public async Task<IEnumerable<Property>> GetProperties(
+    public async Task<IQueryable<Property>> GetProperties(
+        ClaimsPrincipal user,
         [Service] IPropertyService service)
     {
-        return await service.GetPropertiesAsync();
+        var orgIdRaw = user.FindFirst("organization_id")?.Value;
+
+        if (string.IsNullOrWhiteSpace(orgIdRaw) || !int.TryParse(orgIdRaw, out var orgId))
+        {
+            throw new GraphQLException(
+                ErrorBuilder.New()
+                    .SetMessage("Missing or invalid organization_id in token.")
+                    .SetCode("ORG_ID_MISSING")
+                    .Build());
+        }
+
+        var list = await service.GetPropertiesAsync();
+
+        return list.AsQueryable();
     }
 }
